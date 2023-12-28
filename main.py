586d3c2a20184724a58867870bfd479dd2bed4e0
BaAniq <basia.anisimowicz@gmail.com>
2023-12-28 09:30:27 +0100

    Add a check_in and destroy_view function

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from tkinter import *
 
 beige = '#FAF6F0'
-
+menu_listbox = None
+labels_to_delete = None
 
 # creating a  main window
 
@@ -19,15 +20,33 @@ canvas_bg.grid(column=1, row=1, rowspan=3)
 
 
 def show_menu():
-    def menu_list_box_used(event):
-        print(menu_listbox.get(menu_listbox.curselection()))
+    global menu_listbox, labels_to_delete
     menu_listbox = Listbox(height=4)
     menu_elements = ["Show the Guest List", "Check in guest", "Check out guest", "Edit data"]
     for element in menu_elements:
         menu_listbox.insert(menu_elements.index(element), element)
     menu_listbox.bind("<<ListboxSelect>>", menu_list_box_used)
     menu_listbox.grid(column=3, row=2)
+    labels_to_delete = [menu_listbox]
 
+def menu_list_box_used(event):
+    global menu_listbox
+    choice = menu_listbox.get(menu_listbox.curselection())
+    if choice == "Show the Guest List":
+        pass
+    elif choice == "Check in guest":
+        check_in_guest()
+def check_in_guest():
+    destroy_view()
+    name_label = Label(text='Name', font=('Calibre', 10), bg=beige, padx=3)
+    name_label.grid(column=2, row=1)
+
+    name_textbox = Entry(width=8)
+    name_textbox.grid(column=3, row=1)
+def destroy_view():
+    global labels_to_delete
+    for label in labels_to_delete:
+        label.destroy()
 
 show_menu()
 
